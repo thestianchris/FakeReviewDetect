d7d567045a212c225170b8c9f73ef49c827f184e
Christian Gauthier <thestianchris@gmail.com>
2022-12-12 16:03:39 +0000

    update on models+ new cnn model

--- chunkify.py
+++ chunkify.py
@@ -1,11 +1,12 @@
 import io
-import ijson
 import pandas as pd
 
-chunks = pd.read_json('yelp_academic_dataset_review.json', lines=True, chunksize = 20000)
+chunks = pd.read_json('yelp_academic_dataset_review.json', lines=True, chunksize = 50000)
 reviews = pd.DataFrame()
 for chunk in chunks:
   reviews = pd.concat([reviews, chunk])
   print(reviews.iloc[0].text)
   break
 
+
+reviews.savetxt(r'50kreview.txt', header=None, index=None, sep=' ')
